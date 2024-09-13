431814b21714ae9ac15286d1421dcab6a4bf3375
alex cesar <thealexcesar@gmail.com>
2024-09-13 15:05:04 -0300

    default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,13 @@
 import { Component } from '@angular/core';
-import {Router, RouterOutlet} from '@angular/router';
-import {Auth} from "./core/models/user/user.module";
-import {AuthService} from "./core/services/auth.service";
-import {FormsModule} from "@angular/forms";
-import {environment} from "../environments/environment";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title: string = 'Auth Clinic'
-
-  login: Auth = { username: '', password: '' }
-
-  constructor(public authService: AuthService, private router: Router) {
-    console.log(environment.apiUrl)
-  }
-
-  onSubmit(): void {
-    this.authService.authenticate(this.login).subscribe({
-      next: (data: Auth | any) => {
-        localStorage.setItem("token", data.token)
-        this.router.navigateByUrl("/")
-      }, error: error => console.log(error)
-    })
-  }
 }
